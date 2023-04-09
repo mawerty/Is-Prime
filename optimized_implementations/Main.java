e3bc2020ee1daff3a73778276e20b020982cbe6d
Hassan <92521687+divizn@users.noreply.github.com>
2023-04-09 22:10:06 +0100

    Remove whitespaces

--- optimized_implementations/Main.java
+++ optimized_implementations/Main.java
@@ -1,1 +1,1 @@
-public class Main {public static boolean alwaysFalse(int number) {return false;}public static void main(String[] args) {}}
+public class Main{public static boolean alwaysFalse(int number){return false;}public static void main(String[] args){}}
