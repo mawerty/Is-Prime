c7f23e867bd75d7f7fb9466156ebaa1290b3e519
Krzysztof Pniaczek <76811312+mawerty@users.noreply.github.com>
2023-04-10 00:29:31 +0200

    Update Main.java

--- optimized_implementations/Main.java
+++ optimized_implementations/Main.java
@@ -1,1 +1,1 @@
-public class Main{public static boolean alwaysFalse(int number){return false;}public static void main(String[] args){}}
+public class Main{public static boolean is_prime(int number){return false;}public static void main(String[] args){}}
