449471a3c1c232d6ca78d1653e55a5ef70b8969d
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2024-01-12 11:17:12 +0900

    a little bit more optimizations for SystemVerilog

--- optimized_implementations/main.sv
+++ optimized_implementations/main.sv
@@ -1,3 +1,1 @@
-module Main #( parameter N = 32 )( input logic [N-1:0] x, output logic is_prime );
-  assign is_prime = 1'b0;
-endmodule
+module Main #(parameter N=32)(input logic [N-1:0] x,output logic is_prime); assign is_prime = 1'b0; endmodule
